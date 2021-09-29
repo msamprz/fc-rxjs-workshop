d737fa063f5d20c457253d51dca2090575ee7efd
Mahdi <m.pourziaei@gmail.com>
2021-09-29 12:36:51 +0200

    Add unknown sources

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+
+  private taskStarts = new Observable();
+  private taskCompletes = new Observable();
+  private showSpinner = new Observable();
 
   constructor() { }
 }
